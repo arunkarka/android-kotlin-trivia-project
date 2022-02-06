fa10cf933ef42585fd604aad26713b1352fff3d9
arunkarka <arunkarka@gmail.com>
2022-02-06 17:53:49 -0600

    Added nav host fragment to the main layout file. Added title fragment as start destination and added game fragment adjacent to title fragment in navigation graph editor. Connected title fragment to game fragment visually in the editor. Created on click listener for "Play" button on Title fragment and used the navigation action to hook play button to launch the second game fragment.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -22,6 +23,9 @@ class TitleFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment3)
+        )
         return binding.root
     }
 
